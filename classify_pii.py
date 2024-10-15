4655e30221af683ecaace75f019a459eb105be86
Alex Crumley <acrumle1@vols.utk.edu>
2024-10-15 14:30:35 -0400

    updated pipeline to run over dataset and filter based on category and thresh conditions

--- classify_pii.py
+++ classify_pii.py
@@ -1,16 +1,47 @@
-from transformers import pipeline
+from transformers import pipeline, AutoConfig
 import os
 import pandas as pd
 
-gen = pipeline("token-classification", "lakshyakh93/deberta_finetuned_pii", device=-1)
+pii_categories = [
+    'FIRSTNAME', 
+    'LASTNAME', 
+    'EMAIL', 
+    'PHONE_NUMBER', 
+    'USERNAME', 
+    'JOBTITLE', 
+    'COMPANY_NAME', 
+    'ACCOUNTNUMBER', 
+    'ACCOUNTNAME', 
+    'IBAN', 
+    'CREDITCARDNUMBER', 
+    'CREDITCARDCVV', 
+    'STREETADDRESS', 
+    'CITY', 
+    'ZIPCODE', 
+    'STATE', 
+    'SSN', 
+    'PASSWORD', 
+    'URL', 
+    'IP', 
+    'USERAGENT', 
+    'SEX', 
+    'GENDER' 
+]
+
+class_threshold = .95
+model_name = 'lakshyakh93/deberta_finetuned_pii'
+gen = pipeline("token-classification", model_name, device=-1)
 
 file_path = './phishing_emails_merged_filtered.csv'
 #df = safe_load_csv(file_path)
 df = pd.read_csv(file_path)
-df_small = df.head(2)
+df_small = df.head(10)
 del df
 
 for body in df_small['body']:
     output = gen(body, aggregation_strategy="first")
-    print(output)
+    for  pii in output:
+        if pii['score'] > class_threshold:
+            if pii['entity_group'] in pii_categories:
+                print(pii)
     print('\n\n')
